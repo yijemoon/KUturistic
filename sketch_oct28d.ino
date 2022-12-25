d9bb4addb2e8923a0ff3ba771fdf93dd41c875ee
Yije Moon <happyyije@konkuk.ac.kr>
2022-12-25 22:08:24 +0900

    second commit

--- sketch_oct28d.ino
+++ sketch_oct28d.ino
@@ -24,16 +24,19 @@ double AmpsRMS = 0;
 #define IN3 5
 #define IN4 21
 
+//로봇 이동 모터 제어
 AccelStepper stepper1 = AccelStepper(1, STEP1, DIR1);
 AccelStepper stepper2 = AccelStepper(1, STEP2, DIR2);
 MultiStepper steppers;
 
+//케이블 모터 제어
 Stepper cableStepper(stepsPerRevolution,IN1,IN3,IN2,IN4);
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("bluetoothTest BT");
+  SerialBT.begin("bluetoothTest BT"); //블루투스 기능
   
+  //로봇 이동 모터 제어_전진을 위해 양쪽 모터 회전 방향 반대로 설정(50, -50)
   stepper1.setMaxSpeed(200); 
   stepper1.setCurrentPosition(0);
   stepper1.setSpeed(-50);
@@ -53,7 +56,8 @@ void loop() {
    Serial.write(SerialBT.read());
    
    switch (controlKey) {
-    case 'A': { //어댑터 작동 불가(모형임)
+    //모형에 주차 자리 A,B,C만 설치
+    case 'A': { 
       Serial.println(controlKey);
       long positions[] = {0,0};
       steppers.moveTo(positions);
@@ -104,12 +108,10 @@ void loop() {
   }
 }
 
+//케이블 내려 충전 진행 후 케이블 올리기
 void Charging() {
   // 시계 방향으로 한바퀴 회전-> 케이블 내리기
   Serial.println("cable down");
-  /*for(int cnt=0; cnt<3; cnt++) {
-  cableStepper.step(stepsPerRevolution);
-  }*/
   cableStepper.step(stepsPerRevolution*2);
   delay(1000);
   //케이블 어댑터와 연결
@@ -124,22 +126,19 @@ void Charging() {
     Serial.println(" A");
     
     //전류가 0.45이하이면 케이블 올리는 코드 실행 / 그 외엔 계속 전류 측정
-    if(AmpsRMS <= 0.45){ //너무 높게 잡은듯.. //0.25
+    if(AmpsRMS <= 0.45){ 
       Serial.println("Charging Complete");
       Serial.println("cable up");
       // 시계 반대 방향으로 한바퀴 회전-> 케이블 올리기
-      /*for(int cnt=0; cnt<3; cnt++) {
-      cableStepper.step(-stepsPerRevolution);
-      }*/
       cableStepper.step(-stepsPerRevolution*2);
       delay(1000);
       break;
     }
-    //break;
   }
   delay(1000);
 }
 
+//완충신호를 위한 전류측정
 float getVPP()
 {
   float result;
@@ -164,6 +163,5 @@ float getVPP()
    return result;
  }
 
-
 //좌석별 좌표값 : A{0,0} B{-500,500} c{-1000,1000} D{-1500,1500} E{-2000,2000}
-//현재는 좌석사이 거리 500으로 설정했지만 실제 주차장에 설치후 좌표값 재설정할것
+//좌석사이 거리 500으로 설정
